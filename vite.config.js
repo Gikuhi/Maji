17487af12adcf8292068398ba50a57925685d5e1
Gikuhi <richardgikm@gmail.com>
2024-05-29 12:51:26 +0300

    Updates

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+module.exports = {
+  plugins: [
+    {
+      name: '@vite-plugins/react',
+      configure: ({ redirect }) => {
+        redirect.setup({
+          to: 'index.html',
+        })
+      },
+    },
+  ],
+}
