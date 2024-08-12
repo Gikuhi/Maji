b545bf7f809b1b99d00ee0913f3b1d128c83985c
Gikuhi <richardgikm@gmail.com>
2024-08-12 12:34:21 +0300

    Updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,8 @@ export default function Home(){
 <div className="navbar bg-base-100 container mx-auto">
   <div className="navbar-start flex justify-between w-full lg:w-auto">
 
-    {/* Mobile Dropdown Menu */}
-    <div className="dropdown lg:hidden">
+   {/* Mobile Dropdown Menu */}
+   <div className="dropdown lg:hidden">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" />
@@ -31,7 +31,7 @@ export default function Home(){
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" />
         </svg>
       </div>
-      <ul tabIndex={0} className="menu menu-horizontal p-0 space-x-4">
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
         <li><a href="/"><span className="menu-text">Home</span></a></li>
         <li><a href="/products"><span className="menu-text">Products</span></a></li>
         <li><a href="/solutions"><span className="menu-text">Solutions</span></a></li>
@@ -42,6 +42,7 @@ export default function Home(){
       </ul>
     </div>
   </div>
+
 
   <div className="navbar-center">
     <a className="w-fit h-fit btn btn-ghost text-xs">
